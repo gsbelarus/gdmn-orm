98749170d131e9b46085df0dba5f4f03960581a8
Andrej Kirejeŭ <gs1994@gmail.com>
2018-03-28 12:48:03 +0300

    wip

--- src/ERModel.ts
+++ src/ERModel.ts
@@ -2,33 +2,85 @@
  *
  */
 
- export class Attribute {
-   readonly name: string;
-   readonly lName: string;
+import { DBStructure } from 'gdmn-db';
 
-   constructor(name: string, lName: string) {
-     this.name = name;
-     this.lName = lName;
-   }
- }
+export class Attribute {
+  readonly name: string;
+  readonly lName: string;
 
- export class AttributeLink extends Attribute {
- }
+  constructor(name: string, lName: string) {
+    this.name = name;
+    this.lName = lName;
+  }
+}
 
- export class AttributeSet extends Attribute {
- }
+export class Field extends Attribute {
+  readonly fieldName: string;
+  readonly notNull: boolean;
+  readonly position: number;
 
+  constructor(name: string, lName: string, fieldName: string, notNull: boolean) {
+    super(name, lName);
+    this.fieldName = fieldName;
+    this.notNull = notNull;
+  }
+}
 
- export class Entity {
-   readonly parent?: Entity;
-   readonly relName: string;
-   readonly lName: string;
-   readonly attributes: Attribute[];
+export class Constraint extends Attribute {
+  readonly constraintName: string;
+  readonly attributes: Field[];
+}
 
-   constructor(parent: Entity | undefined, relName: string, lName: string) {
-     this.parent = parent;
-     this.relName = relName;
-     this.lName = lName;
-     this.attributes = [];
-   }
- }
+export class PrimaryKey extends Constraint {
+}
+
+export class ForeignKey extends Constraint {
+  readonly references: Entity;
+}
+
+export class UniqueKey extends Constraint {
+}
+
+export class StringField extends Field {
+}
+
+export class NumericField extends Field {
+}
+
+export class SetAttribute extends Attribute {
+  readonly associativeEntity: Entity;
+}
+
+export class WeakAtribute extends Attribute {
+  readonly weakEntity: Entity;
+}
+
+interface Attributes {
+  [name: string]: Attribute
+}
+
+export class Entity {
+  readonly parent?: Entity;
+  readonly relName: string;
+  readonly lName: string;
+  readonly attributes: Attributes;
+
+  constructor(parent: Entity | undefined, relName: string, lName: string) {
+    this.parent = parent;
+    this.relName = relName;
+    this.lName = lName;
+    this.attributes = {};
+  }
+}
+
+interface Entities {
+  [name: string]: Entity
+}
+
+export class ERModel {
+  entities: Entities = {};
+
+  load (dbStruct: DBStructure) {
+
+  }
+}
