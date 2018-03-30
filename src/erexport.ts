4b2c6dba8ada5b5d2b98ad848f4005dafa9e5b80
Andrej Kirejeŭ <gs1994@gmail.com>
2018-03-30 11:36:48 +0300

    new file

--- src/erexport.ts
+++ src/erexport.ts
@@ -1,1 +1,11 @@
 import { DBStructure } from 'gdmn-db';
+import { ERModel } from './ermodel';
+
+export function erExport(dbs: DBStructure) {
+
+  const erModel = new ERModel();
+
+
+
+  return erModel;
+}
