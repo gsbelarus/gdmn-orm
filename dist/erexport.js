714f4b94e0ca6e5f7041e35b9206060c722163c4
Andrej Kirejeŭ <gs1994@gmail.com>
2018-04-01 19:35:50 +0300

    wip

--- dist/erexport.js
+++ dist/erexport.js
@@ -2,10 +2,24 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ermodel_1 = require("./ermodel");
 function erExport(dbs, erModel) {
-    for (const r in dbs.relations) {
-        const relation = dbs.relations[r];
-        erModel.add(new ermodel_1.Entity(undefined, relation.name, relation.name));
+    const fieldRef = (fieldName, fk) => {
+        for (const cName in fk) {
+            if (fk[cName].fields.find(f => f === fieldName)) {
+                return true;
+            }
+        }
+        return false;
+    };
+    for (const relationName in dbs.relations) {
+        const r = dbs.relations[relationName];
+        if (r.primaryKey
+            && r.primaryKey.fields.length === 1
+            && r.primaryKey.fields[0] === 'ID'
+            && !fieldRef('ID', r.foreignKeys)) {
+            erModel.add(new ermodel_1.Entity(undefined, relationName, relationName));
+        }
     }
+    ;
     return erModel;
 }
 exports.erExport = erExport;
--- src/erexport.ts
+++ src/erexport.ts
@@ -1,12 +1,28 @@
-import { DBStructure } from 'gdmn-db';
+import { DBStructure, IRefConstraints } from 'gdmn-db';
 import { ERModel, Entity } from './ermodel';
 
 export function erExport(dbs: DBStructure, erModel: ERModel) {
 
-  for (const r in dbs.relations) {
-    const relation = dbs.relations[r];
-    erModel.add(new Entity(undefined, relation.name, relation.name));
-  }
+  const fieldRef = (fieldName: string, fk: IRefConstraints) => {
+    for (const cName in fk) {
+      if (fk[cName].fields.find( f => f === fieldName )) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  for (const relationName in dbs.relations) {
+    const r = dbs.relations[relationName];
+
+    if (r.primaryKey
+      && r.primaryKey.fields.length === 1
+      && r.primaryKey.fields[0] === 'ID'
+      && !fieldRef('ID', r.foreignKeys))
+    {
+      erModel.add(new Entity(undefined, relationName, relationName));
+    }
+  };
 
   return erModel;
 }
