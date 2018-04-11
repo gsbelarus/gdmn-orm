ccedbb59265995fd46ca58285294beea0efd0f47
Andrej Kirejeŭ <gs1994@gmail.com>
2018-04-11 12:22:57 +0300

    wip

---
+++ dist/definitions/interfaces.d.ts
@@ -0,0 +1,12 @@
+export interface IAttribute {
+    name: string;
+    type: string;
+    references?: string[];
+}
+export interface IEntity {
+    name: string;
+    attributes: IAttribute[];
+}
+export interface IERModel {
+    entities: IEntity[];
+}
