83ea1cb2e3c23b505fb8b446c9f6c391434c1cb0
Andrej Kirejeŭ <gs1994@gmail.com>
2018-04-20 17:53:17 +0300

    wip

--- dist/definitions/erexport.d.ts
+++
@@ -1,3 +0,0 @@
-import { DBStructure, ATransaction } from 'gdmn-db';
-import * as erm from './ermodel';
-export declare function erExport(dbs: DBStructure, transaction: ATransaction, erModel: erm.ERModel): Promise<erm.ERModel>;
