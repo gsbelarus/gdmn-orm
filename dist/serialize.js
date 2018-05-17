b86cc9c8ea8495d9bd265566231d8e51bd278498
andreik <gs1994@gmail.com>
2018-05-17 13:18:18 +0300

    wip

--- dist/serialize.js
+++ dist/serialize.js
@@ -3,8 +3,29 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ermodel_1 = require("./ermodel");
 function loadERModel(serialized) {
     const erModel = new ermodel_1.ERModel();
-    serialized.entities.forEach(e => {
-    });
+    const createEntity = (e) => {
+        let result = erModel.entities[e.name];
+        if (!result) {
+            let parent = undefined;
+            if (e.parent) {
+                const pe = serialized.entities.find(p => p.name === e.parent);
+                if (!pe) {
+                    throw new Error(`Unknown entity ${e.parent}`);
+                }
+                parent = createEntity(pe);
+            }
+            switch (e.className) {
+                case 'Entity':
+                    result = new ermodel_1.Entity(parent, e.name, e.lName, e.isAbstract);
+                    break;
+                default:
+                    throw new Error(`Unknown entity class ${e.className}`);
+            }
+            erModel.add(result);
+        }
+        return result;
+    };
+    serialized.entities.forEach(e => createEntity(e));
     return erModel;
 }
 //# sourceMappingURL=serialize.js.map
--- src/serialize.ts
+++ src/serialize.ts
@@ -1,5 +1,5 @@
 import { LName } from './types';
-import { ERModel } from './ermodel';
+import { ERModel, Entity } from './ermodel';
 
 export interface IAttribute {
   name: string;
@@ -27,9 +27,38 @@ export interface IERModel {
 function loadERModel(serialized: IERModel): ERModel {
   const erModel = new ERModel();
 
-  serialized.entities.forEach( e => {
+  const createEntity = (e: IEntity): Entity => {
+    let result = erModel.entities[e.name];
 
-  });
+    if (!result) {
+      let parent: Entity | undefined = undefined;
+
+      if (e.parent) {
+        const pe = serialized.entities.find( p => p.name === e.parent );
+
+        if (!pe) {
+          throw new Error(`Unknown entity ${e.parent}`);
+        }
+
+        parent = createEntity(pe);
+      }
+
+      switch (e.className) {
+        case 'Entity':
+          result = new Entity(parent, e.name, e.lName, e.isAbstract);
+          break;
+
+        default:
+          throw new Error(`Unknown entity class ${e.className}`);
+      }
+
+      erModel.add(result);
+    }
+
+    return result;
+  };
+
+  serialized.entities.forEach( e => createEntity(e) );
 
   return erModel;
 }
